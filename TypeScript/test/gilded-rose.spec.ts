ffd7cafa432651586f3012fdcee646f69005aa18
Arthur <arthurvandelanoitte@live.fr>
2022-10-28 09:45:09 +0200

    fixed test & update class functions

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -1,54 +1,57 @@
 import { testData } from './data/gilded-rose.data'
-import { PerishableItem } from '../src/class/perishableItem'
-import { AgingItem } from '../src/class/agingItem'
-import { LegendaryItem } from '../src/class/legendaryItem'
-import { LimitedItem } from '../src/class/limitedItem'
-import { Item } from '../src/class/item'
-import { Shop } from '../src/class/shop'
+import { PerishableItem } from '../src/items/perishableItem'
+import { AgingItem } from '../src/items/agingItem'
+import { LegendaryItem } from '../src/items/legendaryItem'
+import { LimitedItem } from '../src/items/limitedItem'
+import { Item } from '../src/items/item'
+import { Shop } from '../src/items/shop'
 
 describe('Gilded Rose', () => {
     let items: Item[], shop: Shop
     items = [
         new PerishableItem('Lettuce', 50, 20),
-        new LegendaryItem('Sulfuras, Hand of Ragnaros', 50, 80),
+        new LegendaryItem('Sulfuras, Hand of Ragnaros'),
         new AgingItem('Aged Brie', 32, 20),
         new LimitedItem('Backstage passes to a TAFKAL80ETC concert', 12, 20),
         new PerishableItem('Cake', 0, 0),
         new PerishableItem('Cake past', -1, 9),
-        new LimitedItem('concert ticket outdated', 0, 20),
+        new LimitedItem('concert ticket outdated', -1, 20),
         new LimitedItem('Backstage ticket in 9 days', 9, 9),
         new LimitedItem('Backstage ticket in 5 days', 5, 34),
         new AgingItem('Red wine', 20, 50)
     ]
-    beforeEach(() => {
+    beforeAll(() => {
         shop = new Shop(items)
         shop.updateInventory()
     })
     it('shouldn\'t have negative quality', () => {
         expect(shop.items[4].quality).toBe(0)
     })
-    // it('normal decrease quality', () => {
-    //     expect(shop.items[0].quality).toBe(19)
-    // })
-    // it('double decrease in quality', () => {
-    //     expect(shop.items[5].quality).toBe(7)
-    // })
-    // it('no decrease in quality', () => {
-    //     expect(shop.items[1].quality).toBe(80)
-    // })
-    // it('normal increase in quality', () => {
-    //     expect(shop.items[2].quality).toBe(21)
-    // })
-    // it('double increase in quality', () => {
-    //     expect(shop.items[7].quality).toBe(11)
-    // })
-    // it('triple increase in quality', () => {
-    //     expect(shop.items[8].quality).toBe(37)
-    // })
-    // it('quality is 0', () => {
-    //     expect(shop.items[6].quality).toBe(0)
-    // })
-    // it('no quality > 50', () => {
-    //     expect(shop.items[9].quality).toBe(50)
-    // })
+    it('should decrease quality by 1', () => {
+        expect(shop.items[0].quality).toBe(19)
+    })
+    it('should decrease quality by 2', () => {
+        expect(shop.items[5].quality).toBe(7)
+    })
+    it('shouldn\'t decrease in quality', () => {
+        expect(shop.items[1].quality).toBe(80)
+    })
+    it('should increase quality by 1 for aging item', () => {
+        expect(shop.items[2].quality).toBe(21)
+    })
+    it('should increase quality by 1 for limited item', () => {
+        expect(shop.items[2].quality).toBe(21)
+    })
+    it('should increase quality by 2 for limited item', () => {
+        expect(shop.items[7].quality).toBe(11)
+    })
+    it('should increase quality by 3 for limited item', () => {
+        expect(shop.items[8].quality).toBe(37)
+    })
+    it('should set quality to 0 for limited item', () => {
+        expect(shop.items[6].quality).toBe(0)
+    })
+    it('shouldn\'t increase quality over 50', () => {
+        expect(shop.items[9].quality).toBe(50)
+    })
 })
