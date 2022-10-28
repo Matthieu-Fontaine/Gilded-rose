c4ad5c192336df3aa47490a338b064ba8529b8c3
Arthur <arthurvandelanoitte@live.fr>
2022-10-28 09:56:08 +0200

    new class + tests & fix tests

---
+++ TypeScript/src/items/conjuredItem.ts
@@ -0,0 +1,23 @@
+import { Item } from './item';
+
+export class ConjuredItem extends Item {
+  constructor(name: string, sellIn: number, quality: number) {
+    super(name, sellIn, quality);
+  }
+
+  update(): void {
+    this.updateQuality();
+    this.updateSellIn();
+  }
+
+  updateQuality(): void {
+    this.quality -= this.sellIn < 0 ? 4 : 2;
+    if (this.quality < 0)
+      this.quality = 0;
+  }
+
+  updateSellIn(): void {
+    this.sellIn--
+  }
+
+}
