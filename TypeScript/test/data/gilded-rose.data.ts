d3f54fdc971d41d585162a9bd16c590fdbab86d9
Awheart <arthurvandelanoitte@live.fr>
2022-12-09 20:20:09 +0100

    remove useless files

--- TypeScript/test/data/gilded-rose.data.ts
+++
@@ -1,8 +0,0 @@
-export const testData = {
-  perishableItem: {
-    mocks: {},
-    inputs: { name: 'Lettuce', sellIn: 50, quality: 20 },
-    expects: { name: 'Lettuce', sellIn: 49, quality: 19 }
-  }
-}
-
